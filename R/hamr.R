e199902efef3f526b2c2e3842dcdfdfcb069cd0f
QUARTZRAINNIE\Duong <dqvu@umail.iu.edu>
2018-03-02 17:57:13 -0800

    Adding RD file for compare_model

--- R/hamr.R
+++ R/hamr.R
@@ -1,4 +1,17 @@
-library(testthat)
-library(hamr)
+# Storing all function
 
-test_check("hamr")
+# JD function
+
+# Linsey function
+
+# Compare_model function
+# A summary function that compares summary statistics between various imputation methods
+compare_model <- function(feature, methods=c("CC","IMP")){
+  
+
+assert feature != None "Missing feature"
+assert isinstance(methods, list) or isinstance(methods, str) 
+"Input method(s) is not in the right type"
+assert isinstance(feature, pd.DataFrame) or isinstance(feature, np.ndarray) 
+"Input feature is not in the right type"
+}
