15fe9d92ed360e37a8bc6e1bc853652ffc5a4b0b
Longlingzi Yao <yllz9610@gmail.com>
2018-03-03 18:42:10 -0800

    add empty function and change unit tests

--- R/hamr.R
+++ R/hamr.R
@@ -2,7 +2,10 @@
 
 # JD function
 
-# Linsey function
+impute_missing <- function(df, method, missing_val_char) {
+# Given a data frame/matrix with missing values, use the specific method to get a complete one
+  
+}
 
 # Compare_model function
 # A summary function that compares summary statistics between various imputation methods
