896256d714327e098cc16346c5eb3361cd0d038d
QUARTZRAINNIE\Duong <dqvu@umail.iu.edu>
2018-03-11 10:38:42 -0700

    Update tests in R

--- tests/testthat/test_compare_model.R
+++ tests/testthat/test_compare_model.R
@@ -24,4 +24,28 @@ test_that("compare_model(feature, method),
     expect_match(typeof(result),"list")
     expect_match(class(result),"data.frame")
     expect_equal(test,result)
+    
+    # expected errors
+    expect_error(compare_model(list(1, 2, 2, NA), "V2", "MIP", "NA"),
+                 "Error: data format is not supported, expected a data frame or a matrix")
+    
+    expect_error(compare_model(data.frame("exp" = c(1, 2, 3), "res" = c(0, 10, "NaN")),
+                                2,
+                                "CC",
+                                NaN),
+                 "Error: column name is not applicable, expected a string instead")
+    
+    expect_error(compare_model(data.frame(exp = c(1, 2, 3), res = c(0, 10, "")),
+                                "dn",
+                                "CC",
+                                ""),
+                 "Error: the specified column name is not in the data frame")
+    
+    expect_error(compare_model(data.frame(x = c(1, 2, 3), y = c(0, 10, NaN)), "y", "multi_im", "NaN"),
+                 "method is not applicable")
+    
+    expect_error(compare_model(data.frame(x = c(1, 2, 3), y = c(0, 10, 0)), "y", "CC", 0),
+                 "Error: missing value format is not supported, expected one of blank space, a question mark, NA and NaN")
 })
+
+
