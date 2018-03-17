15c8a71d7f174e5106a99545df07bea11e990a25
QUARTZRAINNIE\Duong <dqvu@umail.iu.edu>
2018-03-17 09:55:03 -0700

    fix test_compare_models

--- tests/testthat/test_compare_model.R
+++ tests/testthat/test_compare_model.R
@@ -1,25 +1,25 @@
 context("Testing compare_model")
 library(tidyverse)
-df <- read.csv("../dummy_dataset.csv")
+df <- read.csv("../missing_valuesdf.csv")
 
-meds <- c("CC","IMP")
-result <- compare_model('col1', methods = meds)
+meds <- c("CC","MIP")
+result <- compare_model(df,'col1', meds,NA)
 
 test <- broom::tidy(df) %>% 
   select(column,mean,sd,min,median,max) %>% 
   filter(column == "col1")
 
 for(method in meds){
-  df_after <- impute_missing('col1',method)
+  df_after <- impute_missing(df,'col1',method,NA)
   name <- paste('col1_after_', method, sep="")
   b <- broom::tidy(df_after) %>% 
     select(column,mean,sd,min,median,max) %>%
     filter(column == "col1")
-  b['col1'] <- name
+  b$column[1] <- name
   test <- rbind(test,b)
 }
 
-test_that("compare_model(feature, method),
+test_that("compare_model(df,feature, method,missing_val_type),
           Test this function for a normal case", {
     expect_match(typeof(result),"list")
     expect_match(class(result),"data.frame")
@@ -42,7 +42,7 @@ test_that("compare_model(feature, method),
                  "Error: the specified column name is not in the data frame")
     
     expect_error(compare_model(data.frame(x = c(1, 2, 3), y = c(0, 10, NaN)), "y", "multi_im", "NaN"),
-                 "method is not applicable")
+                 "Error: method is not applicable")
     
     expect_error(compare_model(data.frame(x = c(1, 2, 3), y = c(0, 10, 0)), "y", "CC", 0),
                  "Error: missing value format is not supported, expected one of blank space, a question mark, NA and NaN")
